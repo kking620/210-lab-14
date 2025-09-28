54c72d0c36d0e3bb3ec2ced8ca3b11fb36d8dd60
Krispy King <kking620@gmail.com>
2025-09-28 16:43:46 -0700

    Sixth commit

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -1,14 +1,20 @@
+//COMSC-210 | Lab 14 | Kristofer King
+//IDE Used: VSC
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
+//declaring and initializing a class
 class Color
 {
+    //declaring the private elements of the class
     private:
         int Red;
         int Blue;
         int Green;
+
+    //declaring the public elements of the class with setters and getters to declare each value
     public:
         int getRed()
         {
@@ -35,6 +41,7 @@ class Color
             Green = g;
         }
 
+        //an in class print function that can be called to display the values of the colors from within the class
         void print()
         {
             cout << "Red Value: " << Red << endl;
@@ -47,21 +54,30 @@ class Color
 int main()
 {
     cout << "Color #1\n";
+    //declaring the first color class object
     Color color1;
+    //calling the class functions for the class object using the dot operator to set the values of the colors
+    //and printing out the appropriate values
     color1.setRed(155);
     color1.setBlue(76);
     color1.setGreen(103);
     color1.print();
 
     cout << "Color #2\n";
+    //declaring the second color class object
     Color color2;
+    //calling the class functions for the class object using the dot operator to set the values of the colors
+    //and printing out the appropriate values
     color2.setRed(203);
     color2.setBlue(43);
     color2.setGreen(12);
     color2.print();
 
     cout << "Color #3\n";
+    //declaring the second color class object
     Color color3;
+    //calling the class functions for the class object using the dot operator to set the values of the colors
+    //and printing out the appropriate values
     color3.setRed(6);
     color3.setBlue(35);
     color3.setGreen(204);
